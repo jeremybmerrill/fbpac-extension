616b05e5f80202db94425bae4736e185d07409a6
Jeremy B Merrill <jeremybmerrill@gmail.com>
2020-06-08 10:20:29 -0400

    s/Quartz/NYU/g

--- src/components/stories.jsx
+++ src/components/stories.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export const Stories = () => (
   <div id="stories">
     <div className="precis">
-      <h4>The Facebook Political Ad Collector is back up and running!</h4>
-      <p><a href="https://qz.com">Quartz</a>, an online news organization focused on the global economy, is taking over stewardship of the project which was previously maintained by The Globe and Mail and before that, by ProPublica.</p>
+      <h4>The Facebook Political Ad Collector has a new name: Ad Observer</h4>
+      <p>The <a href="https://engineering.nyu.edu/research/online-political-ads-transparency">Online Political Ads Transparency Project</a> at New York University is taking over stewardship of the project which was previously maintained by Quartz, The Globe and Mail and before that, by ProPublica.</p>
       <p>We&rsquo;ve updated the extension to keep pace with Facebook modifications to their website’s code--either their improvements or their efforts to block the ad collector. ProPublica, The Globe and Mail and all other publication partners will continue to have access to the ads submitted through this extension, the privacy rules around the project and extension have not changed, and, same as ProPublica and The Globe and Mail, we will never collect your personal information during this project.</p>
       <p>We anticipate soon adding some exciting new features to the ad collector, in conjunction with researchers at the Algorithmic Transparency Institute and New York University. Stay tuned.</p>
-      <p>Thank you again for your continued support. To learn more about the extension and how Quartz is using the data you contribute to cover online political influence and disinformation, check out the stories below.</p>
+      <p>Thank you again for your continued support. To learn more about the extension and how the data you contribute helps research and reporting on online political influence and disinformation, check out the links below.</p>
     </div>
     <ul>
       <li>
@@ -15,8 +15,8 @@ export const Stories = () => (
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://qz.com/1818398/"
-          >Help us monitor political ads on Facebook &nbsp;→</a>
+            href="https://onlinepoliticaltransparencyproject.org/"
+          >Online Political Ads Transparency Project &nbsp;→</a>
         </p>
       </li>
       <li>
@@ -40,8 +40,8 @@ export const Stories = () => (
 
     </ul>
     <div className="postcis">
-      <p>If you have any questions about Quartz’s Facebook political ads collector, send us an email at <a target="_blank"
-      rel="noopener noreferrer" href="mailto:investigations@qz.com?subject=Feedback%20on%20extension">investigations@qz.com</a>.</p>
+      <p>If you have any questions about the Ad Observer project, send us an email at <a target="_blank"
+      rel="noopener noreferrer" href="mailto:Developers@adobserver.org?subject=Feedback%20on%20extension">Developers@adobserver.org</a>.</p>
     </div>
   </div>
 );
--- src/utils.js
+++ src/utils.js
@@ -9,7 +9,7 @@ export const adForRequest = ad => ({
 
 const endpoint =
   process.env.NODE_ENV === "production"
-    ? "https://fbpac.qz.ai/facebook-ads/ads"
+    ? "https://fbpac.ad-observatory.com/facebook-ads/ads"
     : "http://localhost:8080/facebook-ads/ads";
 
 const headers = language =>
